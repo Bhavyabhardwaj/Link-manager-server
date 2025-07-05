b68eb20da43662e38388a9788058bcbac78d92f5
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2025-07-05 16:39:43 +0530

    auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { jwtUtil } from "../utils";
+import { UnauthorizedError } from "../errors";
+
+export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+    const token = (req.headers.authorization)?.split(" ")[1];
+
+    if (!token) {
+        throw new UnauthorizedError("Token is required");
+    }
+
+    try {
+        const decoded = jwtUtil.verifyToken(token);
+        (req as any).user = decoded;
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
