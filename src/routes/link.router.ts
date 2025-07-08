f99fed5d2b48f781500145fb61965e543f0777e3
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2025-07-08 12:39:42 +0530

    linkRouter

--- src/routes/link.router.ts
+++ src/routes/link.router.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import { linkController } from "../controllers";
 
 const linkRouter = Router();
 
-linkRouter.post('/')
+linkRouter.post('/link', linkController.createLink);
+linkRouter.get('/links', linkController.getLinks);
+linkRouter.get('/link/:id', linkController.getLinkById);
+linkRouter.put('/link/:id', linkController.updateLink);
+linkRouter.delete('/link/:id', linkController.deleteLink);
 
 export default linkRouter;
