cf1600c239ba0a159243b23cf03ed6dffaf96c52
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2025-07-09 11:36:45 +0530

    tested all link routes

--- src/routes/link.router.ts
+++ src/routes/link.router.ts
@@ -7,6 +7,7 @@ linkRouter.post('/create-link', linkController.createLink);
 linkRouter.get('/get-links', linkController.getLinks);
 linkRouter.get('/get-link/:id', linkController.getLinkById);
 linkRouter.put('/update-link/:id', linkController.updateLink);
+
 linkRouter.delete('/delete-link/:id', linkController.deleteLink);
 
 export default linkRouter;
