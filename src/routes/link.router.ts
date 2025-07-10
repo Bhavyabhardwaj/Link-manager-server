52fc98e710d74e79c86631467ca6f5c1b3813d17
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2025-07-10 15:16:58 +0530

    api-docs for link

--- src/routes/link.router.ts
+++ src/routes/link.router.ts
@@ -38,8 +38,72 @@ const linkRouter = Router();
 
 linkRouter.post('/create-link', linkController.createLink);
 
+/**
+ * @swagger
+ * /api/link/get-links:
+ *   get:
+ *     summary: Get all links
+ *     tags: [Link]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Links fetched successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: success
+ *                 message:
+ *                   type: string
+ *                   example: Links fetched successfully
+ *                 data:
+ *                   type: array
+ */
 linkRouter.get('/get-links', linkController.getLinks);
 linkRouter.get('/get-link/:id', linkController.getLinkById);
+/**
+ * @swagger
+ * /api/link/update-link/{id}:
+ *   put:
+ *     summary: Update a link
+ *     tags: [Link]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the link to update
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            required:
+ *              - title
+ *              - url
+ *              - description
+ *            properties:
+ *              title:
+ *                type: string
+ *                example: My Link
+ *              url:
+ *                type: string
+ *                example: https://example.com
+ *              description:
+ *                type: string
+ *                example: A description of the link
+ *     responses:
+ *       200:
+ *         description: Link updated successfully
+ */
 linkRouter.put('/update-link/:id', linkController.updateLink);
 linkRouter.delete('/delete-link/:id', linkController.deleteLink);
 
