f2a04e00cc81af808a5412273782a3eafbffcad7
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2025-07-12 16:51:59 +0530

    github-auth

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -81,13 +81,3 @@ export const signUp = async({username, password, email}: authValidation.SignupIn
         }
     }
 }
-
-export const updateUsername = async (userId: string, newUsername: string) => {
-  await prisma.user.update({
-    where: { id: userId },
-    data: {
-      username: newUsername,
-      usernameChanged: true,
-    },
-  });
-};
