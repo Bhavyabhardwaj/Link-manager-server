db3888afb55d6f314e19fdcd9b615149888bdccd
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2025-07-14 12:52:55 +0530

    google passport init

---
+++ src/config/passportGoogle.ts
@@ -0,0 +1,36 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import prisma from "./db";
+import dotenv from "dotenv";
+dotenv.config();
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+            callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/api/oauth/google/callback",
+        },
+        async (accessToken: string, refreshToken: string, profile: any, done: any) => {
+            try {
+                let user = await prisma.user.findUnique({
+                    where: { googleId: profile.id },
+                });
+                if (!user) {
+                    user = await prisma.user.create({
+                        data: {
+                            googleId: profile.id,
+                            name: profile.displayName,
+                            email: profile.emails?.[0]?.value,
+                            username: profile.username,
+                            password: "",
+                        },
+                    });
+                }
+                return done(null, user);
+            } catch (error) {
+                return done(error);
+            }
+        }
+    )
+)
