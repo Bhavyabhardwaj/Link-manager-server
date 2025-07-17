63b01b013eddc82af379af7533114d155b7f112a
Bhavya Bhardwaj <bbhardwaj303@gmail.com>
2025-07-17 22:16:47 +0530

    updated mig

---
+++ prisma/migrations/20250715162529_updated/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "linkClick" ADD COLUMN     "agent" TEXT,
+ADD COLUMN     "browser" TEXT,
+ADD COLUMN     "city" TEXT,
+ADD COLUMN     "device" TEXT,
+ADD COLUMN     "os" TEXT;
